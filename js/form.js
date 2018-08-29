4b104a7c0906b0a9781019031c7456f9e8b7b351
jcocjin <jaypee@adspark.ph>
2018-08-29 23:06:52 +0800

    fixed the js of the form

--- js/form.js
+++ js/form.js
@@ -87,99 +87,156 @@ $(document).ready(function () {
 	var $question1 = 0;
 	var $question2 = 0;
 
-	$("input#mobileNumber").keypress(function (char) {
-		var charCode = char.which ? char.which : char.keyCode;
-		if (charCode > 31 && (charCode < 48 || charCode > 57)) return false;
-		return true;
-	});
-
-	$('input#mobileNumber').change(function () {
-		if ($(this).val().length == 7) {
-			$validMobileNUmber = 1;
-
-			$("div.error").remove();
-		} else {
-			$validMobileNUmber = 0;
-
-			$("div#mobileNumber").append("<div class='error-display'>incorrect mobile number format.</div>");
-		}
-	});
-
-	$("body").click(function (event) {
-		$('input#question1').change(function () {
-			$("form").children('button').addClass('disable');
-
-			$valid = 0;
-			$question1 = 0;
-			$question2 = 0;
-
-			$('div#question2').css('display', 'none');
-
-			$('div#question3').css('display', 'none');
-			$('div#question4').css('display', 'none');
-			$('div#question6').css('display', 'none');
-
-			$('input#question2').prop('checked', false);
-			$('textarea#question3').val("");
-			$('input#question4').prop('checked', false);
-			$('textarea#question5').val("");
-			$('input#question6').prop('checked', false);
-			$('textarea#question7').val("");
-			$('textarea#question7').prop('disabled', true);
-
-			if ($(this).val() == "No") {
-				$('div#question6').css('display', 'block');
-			} else {
-				$('div#question2').css('display', 'block');
-				$('div#question3').css('display', 'block');
-				$('div#question4').css('display', 'block');
-			}
-		});
-
-		if ($(event.target).closest('div#YES').length == 1) {
-
-			// console.log($(event.target).attr('id'));
-
-			if ($(event.target).attr('id') == "question2") {
-				$question1 = 1;
-			}
-
-			if ($(event.target).attr('id') == "question4") {
-				$question2 = 1;
-			}
-
+	var validate = {
+		form: function form() {
 			if ($question1 == 1 && $question2 == 1) {
 				$valid = 1;
 			}
 
-			console.log($valid);
-		}
-
-		$('input#question6').click(function () {
-			$question7 = 0;
-
-			if ($(this).val() == "Others") {
-				$('textarea#question7').prop('disabled', false);
+			if ($validMobileNUmber == 1 && $valid == 1) {
+				$("form").children('button').prop('disabled', false);
+				$("form").children('button').removeClass('disable');
 			} else {
-				$('textarea#question7').prop('disabled', true);
+				$("form").children('button').prop('disabled', true);
+				$("form").children('button').addClass('disable');
 			}
-		});
-
-		$('textarea#question7').change(function () {
-			$valid = 0;
-
-			if ($(this).val().length >= 1) {
-				$valid = 1;
-			}
-		});
-
-		if ($validMobileNUmber == 1 && $valid == 1) {
-			$("form").children('button').prop('disabled', false);
-			$("form").children('button').removeClass('disable');
-		} else {
-			$("form").children('button').prop('disabled', true);
-			$("form").children('button').addClass('disable');
+		},
+		status: function status() {
+			console.log($validMobileNUmber + " | " + $valid + " / " + $question1 + " | " + $question2);
 		}
+	};
+
+	$("input#mobileNumber").keypress(function ($char) {
+		var $charCode = $char.which ? $char.which : $char.keyCode;
+		if ($charCode > 31 && ($charCode < 48 || $charCode > 57)) return false;
+		return true;
+	});
+
+	$('input#mobileNumber').keyup(function () {
+		$validMobileNUmber = 0;
+
+		if ($(this).val().length == 7) {
+			$validMobileNUmber = 1;
+		}
+
+		validate.form();
+		validate.status(); //testing
+	});
+
+	$('input#mobileNumber').change(function () {
+		$validMobileNUmber = 0;
+
+		if ($(this).val().length == 7) {
+			$validMobileNUmber = 1;
+
+			$("div.error-display").remove();
+		} else if ($(this).val().length == 0) {
+			$("div.error-display").remove();
+		} else {
+			$("div.error-display").remove();
+			$("div#mobileNumber").append("<div class='error-display'>incorrect mobile number format.</div>");
+		}
+
+		validate.form();
+		validate.status(); //testing
+	});
+
+	$('input#question1').change(function () {
+		$("form").children('button').addClass('disable');
+
+		$valid = 0;
+		$question1 = 0;
+		$question2 = 0;
+
+		$('div#question2').css('display', 'none');
+
+		$('div#question3').css('display', 'none');
+		$('div#question4').css('display', 'none');
+		$('div#question6').css('display', 'none');
+
+		$('input#question2').prop('checked', false);
+		$('textarea#question3').val("");
+		$('input#question4').prop('checked', false);
+		$('textarea#question5').val("");
+		$('textarea#question5').prop('disabled', true);
+		$('input#question6').prop('checked', false);
+		$('textarea#question7').val("");
+		$('textarea#question7').prop('disabled', true);
+
+		if ($(this).val() == "No") {
+			$('div#question6').css('display', 'block');
+		} else {
+			$('div#question2').css('display', 'block');
+			$('div#question3').css('display', 'block');
+			$('div#question4').css('display', 'block');
+		}
+
+		validate.form();
+		validate.status(); //testing
+	});
+
+	$('input#question6').click(function () {
+		$question7 = 0;
+		$valid = 0;
+
+		$('textarea#question7').val('');
+
+		if ($(this).val() == "Others") {
+			$('textarea#question7').prop('disabled', false);
+		} else {
+			$valid = 1;
+			$('textarea#question7').prop('disabled', true);
+		}
+
+		validate.form();
+		validate.status(); //testing
+	});
+
+	$('textarea#question7').keyup(function () {
+		if ($(this).val().length >= 1) {
+			$valid = 1;
+		} else {
+			$valid = 0;
+		}
+
+		validate.form();
+		validate.status(); //testing
+	});
+
+	$('input#question2').click(function () {
+		$question1 = 1;
+
+		validate.form();
+		validate.status(); //testing
+	});
+
+	$('input#question4').click(function () {
+		$question2 = 0;
+		$valid = 0;
+
+		$('textarea#question5').val('');
+
+		if ($(this).val() == "No") {
+			$('textarea#question5').prop('disabled', false);
+		} else {
+			$question2 = 1;
+
+			$('textarea#question5').prop('disabled', true);
+		}
+
+		validate.form();
+		validate.status(); //testing
+	});
+
+	$('textarea#question5').keyup(function () {
+		if ($(this).val().length >= 1) {
+			$question2 = 1;
+		} else {
+			$question2 = 0;
+		}
+
+		validate.form();
+		validate.status(); //testing
 	});
 });
 
